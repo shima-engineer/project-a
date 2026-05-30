d2a753ec2775e1a136a222ab16398025ab3f92c1
shima-engineer <zennengineer@gmail.com>
2026-05-30 18:50:27 +0900

    Revert "Feature/toppage layout"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
-import Header from "./components/layouts/Header";
+import Header from "./components/layout/Header";
 
 const notoSansJP = Noto_Sans_JP({
   variable: "--font-noto-sans-jp",
