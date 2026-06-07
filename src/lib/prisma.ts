d2aa2d434dd28d8aae5a8909e73ac9052051b14b
shima-engineer <zennengineer@gmail.com>
2026-06-07 23:14:45 +0900

    prismaClientシングルトン化

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,18 @@
-import { PrismaClient } from "@/src/generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-export { prisma };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    }),
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
