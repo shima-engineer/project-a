811cd9e69903ac30dbe94441917259da6a3be2ca
shima-engineer <zennengineer@gmail.com>
2026-06-14 15:59:25 +0900

    fix:リファクタリング

--- src/features/product/queries/getProduct.ts
+++ src/features/product/queries/getProduct.ts
@@ -1,5 +1,7 @@
 import dayjs from "dayjs";
 import { prisma } from "@/src/lib/prisma";
+import isoWeek from "dayjs/plugin/isoWeek";
+dayjs.extend(isoWeek);
 
 export async function getProducts() {
   return prisma.products.findMany({
@@ -12,65 +14,33 @@ export async function getProducts() {
   });
 }
 
-export async function getTodayProducts() {
-  return prisma.products.findMany({
-    where: {
-      created_at: {
-        gte: dayjs().startOf("day").toDate(),
-      },
-    },
-    include: {
-      categories: true,
-    },
-    orderBy: {
-      upvotes_count: "desc",
-    },
-  });
-}
-
-export async function getWeekProducts() {
-  return prisma.products.findMany({
-    where: {
-      created_at: {
-        gte: dayjs().startOf("week").toDate(),
-      },
-    },
-    include: {
-      categories: true,
-    },
-    orderBy: {
-      upvotes_count: "desc",
-    },
-  });
-}
-
-export async function getMonthProducts() {
-  return prisma.products.findMany({
-    where: {
-      created_at: {
-        gte: dayjs().startOf("month").toDate(),
-      },
-    },
-    include: {
-      categories: true,
-    },
-    orderBy: {
-      upvotes_count: "desc",
-    },
-  });
-}
-
 export async function getProductsByPeriod(
   period: "daily" | "weekly" | "monthly",
 ) {
-  switch (period) {
-    case "daily":
-      return getTodayProducts();
+  const ranges = {
+    daily: {
+      gte: dayjs().startOf("day").toDate(),
+      lte: dayjs().endOf("day").toDate(),
+    },
+    weekly: {
+      gte: dayjs().startOf("isoWeek").toDate(),
+      lte: dayjs().endOf("isoWeek").toDate(),
+    },
+    monthly: {
+      gte: dayjs().startOf("month").toDate(),
+      lte: dayjs().endOf("month").toDate(),
+    },
+  };
 
-    case "weekly":
-      return getWeekProducts();
-
-    case "monthly":
-      return getMonthProducts();
-  }
+  return prisma.products.findMany({
+    where: {
+      created_at: ranges[period],
+    },
+    include: {
+      categories: true,
+    },
+    orderBy: {
+      upvotes_count: "desc",
+    },
+  });
 }
