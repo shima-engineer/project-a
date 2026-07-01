af5618f9735e1ae9ba52e1d3294b36f852f6bd95
shima-engineer <zennengineer@gmail.com>
2026-07-01 21:39:55 +0900

    wip:ログイン実装中

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -1,64 +1,96 @@
+"use client";
+
+import { createClient } from "@/lib/supabase/client";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LoginFormValues, loginSchema } from "./schemas/loginSchema";
+import { toast } from "sonner";
 
 const LoginForm = () => {
-  // React Hook Formの初期化
+  const [loading, setLoading] = useState(false);
+
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
   } = useForm<LoginFormValues>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
-      name: "",
       email: "",
-      message: "",
+      password: "",
     },
   });
 
-  // 送信処理
-  const onSubmit = async (data: LoginFormValues) => {
-    setSubmitStatus("submitting");
+  const handleLoginSubmit = async (data: LoginFormValues) => {
+    setLoading(true);
+
+    const supabase = createClient();
 
     try {
-      // API呼び出し処理...
-      setSubmitStatus("success");
-      reset(); // フォームリセット
+      const { error } = await supabase.auth.signInWithPassword({
+        email: data.email,
+        password: data.password,
+      });
+
+      if (error) {
+        console.error(error.message);
+        toast.error("メールアドレスまたはパスワードが正しくありません。");
+        return;
+      }
+
+      toast.success("ログインに成功しました。");
     } catch (error) {
-      setSubmitStatus("error");
+      console.error(error);
+      toast.error("エラーが発生しました。もう一度お試しください。");
+    } finally {
+      setLoading(false);
     }
   };
   return (
-    <form onSubmit={(e) => e.preventDefault()} className="space-y-4 text-left">
+    <form
+      onSubmit={handleSubmit(handleLoginSubmit)}
+      className="space-y-4 text-left"
+    >
       <div>
-        <label className="text-xs font-semibold text-slate-700">
+        <label
+          htmlFor="email"
+          className="block text-xs font-semibold text-slate-700 mb-2"
+        >
           メールアドレス
         </label>
         <input
           type="email"
+          id="email"
           placeholder="you@example.com"
-          className="mt-1 w-full rounded-lg border border-slate-200 p-2.5 text-sm focus:border-blue-500 focus:outline-none transition"
+          className="w-full rounded-lg border border-slate-200 p-2.5 text-sm focus:border-blue-500 focus:outline-none transition mb-1"
+          {...register("email")}
         />
+        <p className="text-error text-xs">{errors.email?.message}</p>
       </div>
       <div>
-        <label className="text-xs font-semibold text-slate-700">
+        <label
+          htmlFor="password"
+          className="block text-xs font-semibold text-slate-700 mb-2"
+        >
           パスワード
         </label>
         <input
           type="password"
+          id="password"
           placeholder="6文字以上"
-          className="mt-1 w-full rounded-lg border border-slate-200 p-2.5 text-sm focus:border-blue-500 focus:outline-none transition"
+          className="w-full rounded-lg border border-slate-200 p-2.5 text-sm focus:border-blue-500 focus:outline-none transition mb-1"
+          {...register("password")}
         />
+        <p className="text-error text-xs">{errors.password?.message}</p>
       </div>
 
       <button
         type="submit"
-        className="mt-2 w-full rounded-lg bg-[#4f46e5] py-3 text-sm font-semibold text-white hover:bg-[#4338ca] transition shadow-md shadow-indigo-100"
+        disabled={loading}
+        className="mt-2 w-full rounded-lg bg-[#4f46e5] py-3 text-sm font-semibold text-white hover:bg-[#4338ca] transition shadow-md shadow-indigo-100 hover:cursor-pointer"
       >
-        ログイン
+        {loading ? "ログイン中..." : "ログイン"}
       </button>
     </form>
   );
