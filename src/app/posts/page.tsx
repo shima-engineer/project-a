c324a37cc29c83575143f2a180987d05a83129e4
shima-engineer <zennengineer@gmail.com>
2026-07-16 10:45:50 +0900

    fix:微修正

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,8 @@
 import ProductSubmitForm from "@/features/productSubmitForm/ProductSubmitForm";
-import React from "react";
 
 const page = () => {
   return (
-    <main className="flex-1 grid lg:grid-cols-2 max-w-[1600px] mx-auto w-full">
+    <main className="flex-1 grid lg:grid-cols-2 max-w-400 mx-auto w-full">
       <div className="px-4 sm:px-8 lg:px-12 py-8 lg:py-10 max-w-2xl w-full mx-auto">
         <div className="mb-8">
           <h1 className="text-2xl font-bold mb-1">プロダクトを投稿する</h1>
