c4dec89f8cecabc0ff25c59495cafc3087bbe742
shima-engineer <zennengineer@gmail.com>
2026-07-23 18:05:35 +0900

    fix:ボタン婚をコンポーネント化

--- src/features/productSubmitForm/components/SubmitActionBar.tsx
+++ src/features/productSubmitForm/components/SubmitActionBar.tsx
@@ -1,18 +1,14 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 const SubmitActionBar = () => {
   return (
-    <div className="fixed bottom-0 left-0 flex justify-between items-center bg-background/90 w-full z-10  border-t border-border px-4 sm:px-8 md:px-12 py-4 ">
+    <div className="fixed bottom-0 left-0 flex justify-between items-center bg-background/90 gap-2 w-full z-10  border-t border-border px-4 sm:px-8 md:px-12 py-4 ">
       <span className="text-muted-foreground text-xs">
         必須項目を入力してください
       </span>
       <div className="flex gap-2">
-        <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium cursor-pointer transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 disabled:cursor-not-allowed [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2">
-          下書き保存
-        </button>
-        <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium cursor-pointer transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 disabled:cursor-not-allowed [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 shadow-[var(--shadow-glow)]">
-          投稿する
-        </button>
+        <Button variant="outline">下書き保存</Button>
+        <Button>投稿する</Button>
       </div>
     </div>
   );
