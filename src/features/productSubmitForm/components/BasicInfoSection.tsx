3ccde9afc56351b0638f79e656510cc9cee0b526
shima-engineer <zennengineer@gmail.com>
2026-08-07 16:33:55 +0900

    fix:タグは10文字以内で入力するように修正

--- src/features/productSubmitForm/components/BasicInfoSection.tsx
+++ src/features/productSubmitForm/components/BasicInfoSection.tsx
@@ -53,10 +53,10 @@ const BasicInfoSection = () => {
       return;
     }
 
-    if (tag.length > 20) {
+    if (tag.length > 10) {
       setError("productTags", {
         type: "manual",
-        message: "タグは20文字以内で入力してください。",
+        message: "タグは10文字以内で入力してください。",
       });
       return;
     }
