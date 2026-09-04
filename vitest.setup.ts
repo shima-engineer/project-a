6f0ea654d56d07a5ced8b66b1e2a03d59d33ad96
shima-engineer <zennengineer@gmail.com>
2026-09-04 17:42:35 +0900

    fix:テストのsetupファイルの修正

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,9 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { afterEach, vi } from "vitest";
+
+URL.createObjectURL = vi.fn(() => "blob:vitest-mock");
+URL.revokeObjectURL = vi.fn();
 
 afterEach(() => {
   cleanup();
