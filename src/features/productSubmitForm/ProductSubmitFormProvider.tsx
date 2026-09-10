9695d77993b76291413fd64ca745b096c60ad606
shima-engineer <zennengineer@gmail.com>
2026-09-10 10:28:57 +0900

    refactor:FormProviderをpage.tsxに引き上げ

---
+++ src/features/productSubmitForm/ProductSubmitFormProvider.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  ProductSubmitFormInput,
+  productSubmitFormSchema,
+  ProductSubmitFormValues,
+} from "./schema";
+
+const ProductSubmitFormProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const methods = useForm<
+    ProductSubmitFormInput,
+    unknown,
+    ProductSubmitFormValues
+  >({
+    resolver: zodResolver(productSubmitFormSchema),
+    defaultValues: {
+      productName: "",
+      productTagline: "",
+      productWebsite: "",
+      productCategory: "",
+      productTags: [],
+      productDescription: "",
+      productFeatures: "",
+      productTechnology: "",
+      productPlans: "",
+      productThumbnail: undefined,
+      productScreenshots: [],
+    },
+  });
+
+  return <FormProvider {...methods}>{children}</FormProvider>;
+};
+
+export default ProductSubmitFormProvider;
